c6f9a0546bfdb1e7cc6482bc1ba9c051029c744d
Adegbehingbe Oluwakorede Joseph <josephkorede36@gmail.com>
2023-05-01 15:11:58 +0100

    Modified settings.py file

--- todo_notes/settings.py
+++ todo_notes/settings.py
@@ -40,7 +40,7 @@ SECRET_KEY = get_random_secret_key()
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:    
